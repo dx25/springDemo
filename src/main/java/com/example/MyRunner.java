052533a8322a867279c3af843e7df149dbbd1358
silence <dx25@dx25.me>
2017-05-10 00:13:05 +0800

    why can't i autowire this service?????

---
+++ src/main/java/com/example/MyRunner.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import service.PersonService;
+
+/**
+ * Created by silence on 2017/5/9.
+ */
+@Component
+public class MyRunner implements CommandLineRunner {
+
+    @Autowired
+    private PersonService personService;
+    @Override
+    public void run(String... strings) throws Exception {
+        personService.callName();
+    }
+}
--- src/main/java/com/example/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemoApplication.java
@@ -2,17 +2,30 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import service.PersonService;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringDemoApplication {
-	@Autowired
-	private static PersonService personService;
+//	@Autowired
+//	private  PersonService personService_;
+//
+//	static PersonService personService;
+//
+//	@PostConstruct
+//	public void init(){
+//		personService = personService_;
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
-		personService.callName();
+		//personService.callName();
 	}
 
 }
