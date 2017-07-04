d9b3de8be15111d9ac8266aaad01549cb28674a1
silence <dx25@dx25.me>
2017-07-04 23:21:03 +0800

    add getter

--- src/main/java/com/example/bean/Person.java
+++ src/main/java/com/example/bean/Person.java
@@ -8,6 +8,14 @@ public class Person {
 
     private int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public int getAge() {
         return age;
     }
