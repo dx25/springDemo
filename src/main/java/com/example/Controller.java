26304f554482cfc89af1fabcc0f2852262031819
silence <dx25@dx25.me>
2017-07-10 00:10:28 +0800

    add mybatis generator

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -5,7 +5,6 @@ import com.example.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,12 +18,12 @@ public class Controller {
     PersonService personService;
 
     @RequestMapping("/callAll")
-    public List<Person> callAll(){
+    public List<Person> callAll() {
         return personService.callName();
     }
 
     @RequestMapping("/callSb/{id}")
-    public Person callSb(@PathVariable("id") Long id){
+    public Person callSb(@PathVariable("id") Long id) {
         return personService.callSb(id);
     }
 
