4ff9ddcbc5e15c7815e16131395cfd074d4da496
dingxy <dingxy@inmindglobal.com>
2018-02-28 18:21:50 +0800

    add classmates

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -1,34 +1,48 @@
-create table table_user
+CREATE TABLE table_user
 (
-	id int auto_increment
-		primary key,
-	name varchar(10) null,
-	age int null
-)
-;
----mysql version must be above then 5.5
-create table message
-(
-	id int auto_increment
-		primary key,
-	name varchar(50) not null,
-	description varchar(500) null,
-	createdAt timestamp default CURRENT_TIMESTAMP not null,
-	updatedAt timestamp default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP not null,
-	endAt timestamp default '0000-00-00 00:00:00' not null,
-	source varchar(100) null
-)
-;
-
-CREATE TABLE user_token(
-	id BIGINT auto_increment
-		primary key,
-	user_id BIGINT NOT NULL ,
-	token varchar(64) not null,
-	createdAt timestamp default CURRENT_TIMESTAMP not null
+  id   INT AUTO_INCREMENT
+    PRIMARY KEY,
+  name VARCHAR(10) NULL,
+  age  INT         NULL
 );
-CREATE UNIQUE INDEX UNQ_TOKEN_TOKEN ON user_token(token);
-CREATE UNIQUE INDEX UNQ_TOKEN_USER ON user_token(user_id);
+---mysql version must be above THEN 5.5
+CREATE TABLE message
+(
+  id          INT       AUTO_INCREMENT
+    PRIMARY KEY,
+  name        VARCHAR(50)                                                     NOT NULL,
+  description VARCHAR(500)                                                    NULL,
+  createdAt   TIMESTAMP DEFAULT CURRENT_TIMESTAMP                             NOT NULL,
+  updatedAt   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  endAt       TIMESTAMP DEFAULT '0000-00-00 00:00:00'                         NOT NULL,
+  source      VARCHAR(100)                                                    NULL
+);
 
+CREATE TABLE user_token (
+  id        BIGINT AUTO_INCREMENT
+    PRIMARY KEY,
+  user_id   BIGINT                              NOT NULL,
+  token     VARCHAR(64)                         NOT NULL,
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+CREATE UNIQUE INDEX UNQ_TOKEN_TOKEN
+  ON user_token (token);
+CREATE UNIQUE INDEX UNQ_TOKEN_USER
+  ON user_token (user_id);
+
+CREATE TABLE classmates (
+  id               BIGINT AUTO_INCREMENT
+    PRIMARY KEY,
+  name             VARCHAR(32)  NOT NULL,
+  mobile           VARCHAR(128) NULL,
+  email            VARCHAR(128) NULL,
+  birthday         DATETIME     NULL,
+  guaduated_school VARCHAR(128) NULL,
+  major            VARCHAR(128) NULL,
+  double_major     VARCHAR(128) NULL,
+  upgraded_school  VARCHAR(128) NULL,
+  city             VARCHAR(128) NULL,
+  company          VARCHAR(128) NULL
+)
 
 
