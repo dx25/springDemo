803e81f8a45f8f0c23cd8fb1b528698d004bdaaa
dingxy <dx25@dx25.me>
2019-01-27 22:37:15 +0800

    fix vue

--- src/main/resources/META-INF/resources/js/index.js
+++ src/main/resources/META-INF/resources/js/index.js
@@ -8,16 +8,23 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!',
         columns: [
-            {title: 'Index', prop: '$index', width: 100},
-            {title: 'ID', prop: 'id', width: 100, sort: 'auto'},
-            {title: 'Name', prop: 'name', sort: 'auto'},
-            {title: 'Age', prop: 'age', sort: 'auto'},
-            {title: 'Address', prop: 'address'},
+            // {title: 'Index', prop: '$index', width: 100},
+            // {title: 'ID', prop: 'id', width: 100, sort: 'auto'},
+            {title: '姓名', prop: 'name', sort: 'auto'},
+            {title: '手机号', prop: 'mobile', sort: 'auto'},
+            {title: '生日', prop: 'birthday'},
+            {title: '毕业学校', prop: 'guaduatedSchool'},
+            {title: '专业', prop: 'major'},
+            {title: '第二专业', prop: 'doubleMajor'},
+            {title: '升学学校', prop: 'upgradedSchool'},
+            {title: '所在城市', prop: 'city'},
+            {title: '公司', prop: 'company'},
         ],
         datas: [],
     },
     method: {},
     mounted: function () {
+        let that = this
         let myHeaders = new Headers();
         myHeaders.append('Content-Type', 'application/json');
         let req = new Request("/classmates/list", {
@@ -31,7 +38,7 @@ var app = new Vue({
         fetch(req).then(function (resp) {
             if (resp.status == 200 && resp.ok) {
                 resp.json().then(body => {
-                    this.datas = body
+                    that.datas = body
                 })
             }
         })
